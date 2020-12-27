393afa05be6284b8e7c856a078a823a1a8a1b033
zhanibekrysbek <zhanibek.rysbek@gmail.com>
2020-12-26 21:03:58 -0600

    haptic feature extraction (force torque) in script form

--- classification/feature_extraction/feature_extraction.m
+++ classification/feature_extraction/feature_extraction.m
@@ -46,13 +46,18 @@ grid on;
 
 %% Computed Torque
 
-obs = observations_processed(15);
+obs = observations_processed(2);
 
 rv1 = [ 0.2275, 0, -0.015];
 rv2 = [-0.2275, 0, -0.015];
 
 tcomp1 = cross(obs.rft1.torque, repmat(rv1,[numel(obs.rft1.time_steps), 1]));
 tcomp2 = cross(obs.rft2.torque, repmat(rv2,[numel(obs.rft2.time_steps), 1]));
+obs.rft1.tcomp = tcomp1;
+obs.rft2.tcomp = tcomp2;
+obs.rft1.ttorque = obs.rft1.torque+tcomp1;
+obs.rft2.ttorque = obs.rft2.torque+tcomp2;
+obs.fsum.ttsum = obs.rft2.torque+tcomp2 + obs.rft1.torque+tcomp1;
 
 figure(3); 
 subplot(3,3,1);
@@ -92,5 +97,106 @@ set(hL, 'Position',[0.51 0.03 0.01 0.01],'Units','normalized')
 
 
 
+%% Haptics Based features FORCES
+
+dotsv = @(f1,f2) f1(:,1).*f2(:,1) + f1(:,2).*f2(:,2);
+
+% obs = observations_processed(5);
+ax = [1,2];
+eps = 0.01;
+
+Me1fxy = dotsv(obs.rft1.force(:,ax),obs.fsum.force(:,ax))...
+ ./(vecnorm(obs.rft1.force(:,ax),2,2).*vecnorm(obs.fsum.force(:,ax),2,2));
+
+Me2fxy = dotsv(obs.rft2.force(:,ax),obs.fsum.force(:,ax))...
+ ./(vecnorm(obs.rft2.force(:,ax),2,2).*vecnorm(obs.fsum.force(:,ax),2,2));
 
 
+Mtfxy = vecnorm(obs.fsum.force(:,ax),2,2)...
+    ./(vecnorm(obs.rft1.force(:,ax),2,2)+vecnorm(obs.rft2.force(:,ax),2,2));
+
+Mnfxy = vecnorm(obs.fsum.force(:,ax),2,2).^2 ...
+    ./(abs(dotsv(obs.rft1.force(:,ax),obs.fsum.force(:,ax)))...
+     + abs(dotsv(obs.rft2.force(:,ax),obs.fsum.force(:,ax))));
+
+Msfxy = 1 - abs( (abs(dotsv(obs.rft1.force(:,ax),obs.fsum.force(:,ax)))...
+     - abs(dotsv(obs.rft2.force(:,ax),obs.fsum.force(:,ax)))) ...
+     ./vecnorm(obs.fsum.force(:,ax),2,2).^2);
+ 
+ 
+figure(1);
+subplot(411)
+plot(obs.rft1.time_steps, Me1fxy); hold on;
+plot(obs.rft1.time_steps, Me2fxy); hold off;
+subtitle('Individual Efficiency')
+
+subplot(412)
+plot(obs.rft1.time_steps, Mtfxy);
+subtitle('Team Efficiency')
+
+% ylim([0,20])
+subplot(413)
+plot(obs.rft1.time_steps, Mnfxy);
+subtitle('Negotiation Efficiency')
+
+
+subplot(414)
+plot(obs.rft1.time_steps, Msfxy);
+subtitle('Similarity of Forces')
+sgtitle('Haptic Features')
+
+
+figure(2);
+plot_rfts(obs,2)
+
+%% Haptics Based features TORQUES
+
+dotsv = @(f1,f2) f1(:,1).*f2(:,1) + f1(:,2).*f2(:,2);
+
+% obs = observations_processed(5);
+ax = [2,3];
+eps = 0.01;
+
+Me1txy = dotsv(obs.rft1.ttorque(:,ax),obs.fsum.ttsum(:,ax))...
+ ./(vecnorm(obs.rft1.torque(:,ax),2,2).*vecnorm(obs.fsum.ttsum(:,ax),2,2));
+
+Me2txy = dotsv(obs.rft2.ttorque(:,ax),obs.fsum.ttsum(:,ax))...
+ ./(vecnorm(obs.rft2.ttorque(:,ax),2,2).*vecnorm(obs.fsum.ttsum(:,ax),2,2));
+
+
+Mttxy = vecnorm(obs.fsum.ttsum(:,ax),2,2)...
+    ./(vecnorm(obs.rft1.ttorque(:,ax),2,2)+vecnorm(obs.rft2.ttorque(:,ax),2,2));
+
+Mntxy = vecnorm(obs.fsum.ttsum(:,ax),2,2).^2 ...
+    ./(abs(dotsv(obs.rft1.ttorque(:,ax),obs.fsum.ttsum(:,ax)))...
+     + abs(dotsv(obs.rft2.ttorque(:,ax),obs.fsum.ttsum(:,ax))));
+
+Mstxy = 1 - abs( (abs(dotsv(obs.rft1.ttorque(:,ax),obs.fsum.ttsum(:,ax)))...
+     - abs(dotsv(obs.rft2.ttorque(:,ax),obs.fsum.ttsum(:,ax)))) ...
+     ./vecnorm(obs.fsum.ttsum(:,ax),2,2).^2);
+ 
+ 
+figure(1);
+subplot(411)
+plot(obs.rft1.time_steps, Me1txy); hold on;
+plot(obs.rft1.time_steps, Me2txy); hold off;
+subtitle('Individual Efficiency')
+
+subplot(412)
+plot(obs.rft1.time_steps, Mttxy);
+subtitle('Team Efficiency')
+
+% ylim([0,20])
+subplot(413)
+plot(obs.rft1.time_steps, Mntxy);
+subtitle('Negotiation Efficiency')
+
+
+subplot(414)
+plot(obs.rft1.time_steps, Mstxy);
+subtitle('Similarity of Forces')
+sgtitle('Haptic Features')
+
+
+% figure(2);
+% plot_rfts(obs,2)
--- preprocessing/signal_processing/bandpass_fft.m
+++ preprocessing/signal_processing/bandpass_fft.m
@@ -1,4 +1,4 @@
-function xnew = lowpass_fft(x, Fs, cutoff)
+function xnew = bandpass_fft(x, Fs, lcutoff, hcutoff)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     y = fft(x);     % FFT  
@@ -7,7 +7,7 @@ function xnew = lowpass_fft(x, Fs, cutoff)
     fshift = (-n/2:n/2-1)*(Fs/n);
     yshift = fftshift(y);
     % Remove high frequency components
-    I = fshift<=cutoff & fshift>=-cutoff;
+    I = (fshift<=hcutoff & fshift>=lcutoff) | (fshift>=-hcutoff & fshift<=-lcutoff);
     yshift(~I) = 0;
     % Recover the signal from freq domain
     ynew = ifftshift(yshift);
