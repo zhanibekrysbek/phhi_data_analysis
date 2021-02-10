5e1723285fe13d2089ad19fdc202c2283969fe63
zhanibekrysbek <zhanibek.rysbek@gmail.com>
2021-02-10 13:15:18 -0600

    adding information content by feature

--- classification/learning/pca_script.m
+++ classification/learning/pca_script.m
@@ -8,14 +8,15 @@ base_path = '../../data/preprocessed_v2_1';
 feature_inds = [0,30, 60, 95, 125];
 feature_names = {'rft1', 'rft2', 'pose', 'twist', 'accel'};
 
-data_version = {'body_frame', 'spatial_frame'};
+data_version = {'body_frame', 'spatial_frame', 'body_n_haptics'};
 norm_option = {'unnormalized','normalized'};
 
 %% Load Features
 
-data_option = 1;
+data_option = 3;
 
 [X,Y] = extractSWFeatures(observations_processed, data_option);
+[N, numFeats] = size(X);
 
 % Normalize
 Xnorm = 2*(X-min(X))./(max(X)-min(X))-1;
@@ -30,14 +31,45 @@ Xnorm_pca = Xnorm*coeff_norm;
 
 %% Box Plot of the data
 
-I = 1:5:140;
+I = 1:5:numFeats;
 figure(31);
 boxplot(X(:,I))
 title('raw')
+grid on;
 
 figure(32);
 boxplot(Xnorm_pca(:,I))
 title('normalized')
+grid on;
+
+%% Information Content by Components
+
+latent_temp = latent_norm;
+
+totVar = sum(latent_temp);
+latent_temp = latent_temp/totVar;
+latent_integ = zeros(size(latent_temp));
+for i = 1:numel(latent_temp)
+    latent_integ(i) = sum(latent_temp(1:i));
+end
+
+figure(41);
+subplot(2,1,1)
+scatter(1:size(X,2), latent_temp, 'filled', 'blue'); hold on;
+p = plot(1:size(X,2), latent_temp, 'blue', 'LineWidth', 0.1);
+subtitle('Variance by Components')
+p.Color(4) = 0.2;
+hold off; box on;
+ylabel('variance')
+
+subplot(2,1,2)
+scatter(1:size(X,2), latent_integ, 'filled', 'red'); hold on;
+p = plot(1:size(X,2), latent_integ, 'red', 'LineWidth', 0.1);
+subtitle('Variance accumulated')
+p.Color(4) = 0.2;
+hold off; box on;
+xlabel('Principal Components')
+ylabel('Accumulated variance')
 
 %% Visualize coeffs
 % close all;
@@ -58,10 +90,10 @@ for tonorm = 1:2
     for i=1:numF
         subplot(numF,1,i)
 
-        scatter(1:140,C(i,:), 'filled', 'DisplayName', num2str(i), ... 
+        scatter(1:numFeats,C(i,:), 'filled', 'DisplayName', num2str(i), ... 
             'MarkerEdgeColor', cmap(i,:),'MarkerFaceColor', cmap(i,:)); 
         hold on;
-        p = plot(1:140,C(i,:), 'LineWidth', 0.1, 'color', cmap(i,:), 'DisplayName', '');
+        p = plot(1:numFeats,C(i,:), 'LineWidth', 0.1, 'color', cmap(i,:), 'DisplayName', '');
         p.Color(4) = 0.2;
         hold off; grid on; box on;
 
