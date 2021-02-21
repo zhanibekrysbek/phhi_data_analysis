a81fac35012f64c1eef4d5e56aa52061b7c91df8
zhanibekrysbek <zhanibek.rysbek@gmail.com>
2021-02-21 17:01:20 -0600

    adding an option to put vertical line to plot tdec_time

--- preprocessing/visualization/trajvector_plot.m
+++ preprocessing/visualization/trajvector_plot.m
@@ -8,10 +8,11 @@ rv2 = [-0.235, 0, -0.027];
 
 
 inds = 1:step_:numel(obs.pose123.time_steps);
+
 rftinds = 1:step_*10:numel(obs.pose123.time_steps)*10;
 
 % Compute Handle coordinates
-R = axang2rotm(obs.pose123.orientation(inds,:));
+R = axang2rotm(obs.imu.orientation(inds,:));
 P = obs.pose123.position(inds,:);
 H1 = zeros(size(P));
 H2 = H1;
@@ -57,9 +58,9 @@ switch opt
             obs.fsum.forceS(rftinds,1),obs.fsum.forceS(rftinds,2),'b','LineWidth',1.5,...
             'MaxHeadSize',0.05);
 
-        ac = quiver(obs.pose123.position(inds,1),obs.pose123.position(inds,2),...
-            obs.pose123.linacc(inds,1),obs.pose123.linacc(inds,2), 'c', 'MaxHeadSize',0.05,...
-            'DisplayName','accel');
+%         ac = quiver(obs.pose123.position(inds,1),obs.pose123.position(inds,2),...
+%             obs.pose123.linacc(inds,1),obs.pose123.linacc(inds,2), 'c', 'MaxHeadSize',0.05,...
+%             'DisplayName','accel');
 
     case 2
         % RFT and imu.accel in Body Frame
@@ -73,10 +74,46 @@ switch opt
         fsum = quiver(obs.pose123.position(inds,1),obs.pose123.position(inds,2),...
             obs.fsum.force(rftinds,1),obs.fsum.force(rftinds,2),'b','LineWidth',1.5,...
             'MaxHeadSize',0.05);
-        ac = quiver(obs.pose123.position(inds,1),obs.pose123.position(inds,2),...
-            obs.imu.accel(inds,1),obs.imu.accel(inds,2), 'c', 'MaxHeadSize',0.05,...
-            'DisplayName','accel');
+%         ac = quiver(obs.pose123.position(inds,1),obs.pose123.position(inds,2),...
+%             obs.imu.accel(inds,1),obs.imu.accel(inds,2), 'c', 'MaxHeadSize',0.05,...
+%             'DisplayName','accel');
         
+    case 3
+        % RFT in Spatial Frame
+
+
+        % Force1 Vector
+        f1 = quiver(H1(:,1),H1(:,2), obs.rft1.forceS(rftinds,1),obs.rft1.forceS(rftinds,2),...
+            'm','LineWidth',1.5, 'MaxHeadSize',0.05,'ShowArrowHead','on');
+        % Force2 Vector
+        f2 = quiver(H2(:,1),H2(:,2), obs.rft2.forceS(rftinds,1),obs.rft2.forceS(rftinds,2),...
+            'g','LineWidth',1.5,'ShowArrowHead','on', 'MaxHeadSize',0.05);
+        % Fsum Vector
+        fsum = quiver(obs.pose123.position(inds,1),obs.pose123.position(inds,2),...
+            obs.fsum.forceS(rftinds,1),obs.fsum.forceS(rftinds,2),'b','LineWidth',1.5,...
+            'MaxHeadSize',0.05);
+
+%         ac = quiver(obs.pose123.position(inds,1),obs.pose123.position(inds,2),...
+%             obs.pose123.linacc(inds,1),obs.pose123.linacc(inds,2), 'c', 'MaxHeadSize',0.05,...
+%             'DisplayName','accel');
+        
+        
+    case 4
+        % RFT and imu.accel in Body Frame
+        % Force1 Vector
+%         f1 = quiver(H1(:,1),H1(:,2), obs.rft1.force(rftinds,1),obs.rft1.force(rftinds,2),...
+%             'm','LineWidth',1.5, 'MaxHeadSize',0.05,'ShowArrowHead','on');
+%         % Force2 Vector
+%         f2 = quiver(H2(:,1),H2(:,2), obs.rft2.force(rftinds,1),obs.rft2.force(rftinds,2),...
+%             'g','LineWidth',1.5,'ShowArrowHead','on', 'MaxHeadSize',0.05);
+%         % Fsum Vector
+%         fsum = quiver(obs.pose123.position(inds,1),obs.pose123.position(inds,2),...
+%             obs.fsum.force(rftinds,1),obs.fsum.force(rftinds,2),'b','LineWidth',1.5,...
+%             'MaxHeadSize',0.05);
+%         ac = quiver(obs.pose123.position(inds,1),obs.pose123.position(inds,2),...
+%             obs.imu.accel(inds,1),obs.imu.accel(inds,2), 'c', 'MaxHeadSize',0.05,...
+%             'DisplayName','accel');
+
 end
 
 % Plot Objects
@@ -86,6 +123,10 @@ rectangle('Position',[2.5 -0.5 .4 1], 'EdgeColor', 'red', 'LineWidth',1)
 text( 2.65,-0.6, 'B', 'FontSize',14)
 rectangle('Position',[1.25 -0.2 .25 .3], 'EdgeColor', 'black', 'LineWidth',3)
 text( 1.35,-0.05, 'Obs', 'FontSize',12)
+
+
+xline(obs.pos_dec(1),'-.b', 'td',....
+            'LabelHorizontalAlignment', 'center', 'LabelVerticalAlignment', 'bottom','LabelOrientation','horizontal')
 
 
 % Title and axis labels
@@ -103,10 +144,12 @@ xlim([-0.5,3.1]);
 % ylim([-1.5,1.5]);
 axis equal;
 
-legend([tr,ve,ac,f1,f2,fsum], {'trajectory', 'velocity', 'accel','F_1','F_2','F_{sum}'});
+% legend([tr,ve,ac,f1,f2,fsum], {'trajectory', 'velocity', 'accel','F_1','F_2','F_{sum}'});
+legend([tr,ve,f1,f2,fsum], {'trajectory', 'velocity','F_1','F_2','F_{sum}'});
 % legend;
-sgtitle(sprintf('%s    %s    %s    Kinematics tlag:%.2d', obs.obs_id, obs.traj_type, obs.motion_type, ...
-    mean(obs.pose123.time_steps(inds)-obs.rft1.time_steps(rftinds))), 'Interpreter','none');
+
+sgtitle(sprintf('%s    %s    %s    %s ', obs.obs_id, obs.traj_type, ...
+    obs.motion_type, obs.initialOrient), 'Interpreter','none');
 
 
 end
