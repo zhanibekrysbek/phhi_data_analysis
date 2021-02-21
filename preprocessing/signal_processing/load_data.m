bc9387f8ee8f1388d21537c86577ee9a6c2170bd
zhanibekrysbek <zhanibek.rysbek@gmail.com>
2021-02-21 16:56:32 -0600

    manual fix of annotation mistakes

--- preprocessing/signal_processing/load_data.m
+++ preprocessing/signal_processing/load_data.m
@@ -2,6 +2,11 @@ function [observations,tb] = load_data(base_path)
 %load_data Summary of this function goes here
 %   Detailed explanation goes here
     
+    global M
+    
+    subjectID = cell({'KOH'; 'Sanket'; 'Vignesh'; 'Zhanibek'});
+    M = containers.Map(subjectID,[1:4]);
+    
     Nobs = 112;
     files = dir(base_path);
 
@@ -17,6 +22,9 @@ function [observations,tb] = load_data(base_path)
                 observations(ind).(fns{fn}) = ld.(fns{fn});
             end
             observations(ind).duration = observations(ind).rft1.time_steps(end);
+            observations(ind).outcomeSubject = getOutcomeSubject(observations(ind));
+            observations(ind).pos_dec = getPosXdecision(observations(ind));
+            
             ind = ind+1;
         end
     end
@@ -31,5 +39,34 @@ function [observations,tb] = load_data(base_path)
     end
     tb.order = [1:height(tb)]';
     tb = [tb(:,end),tb(:,1:end-1)];
+    
+    % Fixing Typos in Annotations
+    tb(tb.obs_id=='KOH_Sanket_16',:).initialOrient = 'xl';
+    tb(tb.obs_id=='KOH_Sanket_9',:).initialOrient = 'xr';
+    tb(tb.obs_id=='KOH_Sanket_7',:).initialOrient = 'xl';
+    tb(tb.obs_id=='Sanket_Vignesh_1_3',:).initialOrient = 'xl';
+    
+    observations(48).tdec_sec = observations(48).tdec_sec - 0.7;
+    observations(48).pos_dec = getPosXdecision(observations(48));
+        
 end
 
+
+function  sub = getOutcomeSubject(obs)
+    
+    global M
+    
+    if contains(obs.outcome, '1')
+        sub = M(obs.handle_1);
+    else 
+        sub = M(obs.handle_2);
+    end
+end
+
+
+function pos = getPosXdecision(obs)
+    
+    I = obs.pose123.time_steps <= obs.tdec_sec;
+    pos = obs.pose123.position(find(I,1,'last'),:);
+
+end
