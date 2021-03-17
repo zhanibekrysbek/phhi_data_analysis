9079de8cce2f3819066b41dd23b0145fe792aca9
zhanibekrysbek <zhanibek.rysbek@gmail.com>
2021-03-17 16:31:36 -0500

    cluster visualization, heatmaps

--- classification/primitives_clustering/pca_script_prim.m
+++ classification/primitives_clustering/pca_script_prim.m
@@ -11,7 +11,8 @@ norm_option = {'unnormalized','normalized'};
 
 %% Cluster Primtable
 
-X = table2array(primtable(:,[8:end]));
+% X = table2array(primtable(idx_fstr==3,[8:10, 19:27]));
+X = table2array(primtable(:,[9:end]));
 [N, numFeats] = size(X);
 
 % Normalize
@@ -23,7 +24,7 @@ Xnorm = 2*(X-min(X))./(max(X)-min(X))-1;
 
 X_pca = X*coeff;
 Xnorm_pca = Xnorm*coeff_norm;
-
+% Xnorm_pca = coeff_norm*Xnorm;
 
 %% Box Plot of the data
 
@@ -146,7 +147,7 @@ end
 
 %% Visualize outcomeSubject/traj_type/initialOrientation/motion_type
 
-idx_temp = primtable.seqnum;
+idx_temp = (primtable.outcome+1)/2 + 1;
 
 k = numel(unique(idx_temp));
 
@@ -173,7 +174,7 @@ legend(num2str([1:k]'));
 %% Kmeans
 
 
-k = 7;
+k = 3;
 ncomponents = 20;
 [idx_norm_pca,C_norm_pca,sumd_norm,D_norm] = kmeans(Xnorm_pca(:,1:ncomponents),k);
 
@@ -197,7 +198,7 @@ c = zeros(numel(idx_temp),3);
 for i =  1:k%numel(idx_temp)
     c(i,:) = cmap(idx_temp(i),:);
     Is = idx_temp==i;
-    scatter3(Xtemp(Is,1), Xtemp(Is,2), Xtemp(Is,3),10,cmap(i,:)); hold on;
+    scatter3(Xtemp(Is,1), Xtemp(Is,2), Xtemp(Is,3),30,cmap(i,:), 'filled'); hold on;
 end
 
 xlabel('Component 1')
@@ -206,4 +207,136 @@ zlabel('Component 3')
 
 grid on; hold off;
 legend(num2str([1:k]'));
+
+
+%% Cluster visualization
+
+
+figure(77);
+
+% numF = 4;
+cmap = hsv(k);
+% varnames = {'fstr_max', 'fstr_min', 'outcome', 'fsum_max', 'exp_dir', ...
+%         'inst_dec', 'lfcons', 'seqnum'};
+
+varnames = {'fstr_max', 'fstr_min', 'vx_gain', 'vy_gain', ...
+        'pos_x_mean', 'pos_y_mean', 'duration', 'fstr_t_extr', 'fsumx_max',  'lfcons', ...
+        'exp_dir', 'inst_dec', 'seqnum', 'outcome'};
+
+
+for i=1:k
+    
+    cl = primtable(idx_temp==i,:);
+    
+%     clfeat = table2array(cl(:,varnames));
+    clfeat = [ cl.fstr_max(:,1)  cl.fstr_min(:,1)...
+        20*cl.v_gain, 20*cl.pos_mean, 20*cl.duration-20, 20*cl.fstr_t_extr-15, ...
+        3*cl.fsum_max(:,1) 10*cl.seqnum-15 10*cl.exp_dir 20*cl.inst_dec-10 ...
+        20*cl.lfcons-30  10*cl.outcome];
+    
+    numF = size(clfeat,2);
+    
+    N = size(clfeat,1);                                      % Number of ‘Experiments’ In Data Set
+    yMean = mean(clfeat);                                    % Mean Of All Experiments At Each Value Of ‘x’
+    ySEM = std(clfeat)/sqrt(N);                              % Compute ‘Standard Error Of The Mean’ Of All Experiments At Each Value Of ‘x’
+    CI95 = tinv([0.025 0.975], N-1);                         % Calculate 95% Probability Intervals Of t-Distribution
+    yCI95 = bsxfun(@times, ySEM, CI95(:));   
+    
+%     [ median(cl.fstr_max(:,1)) mean(cl.fstr_min(:,1)) 10*mean(cl.outcome) ...
+%         mean(cl.fsum_max) 5*mean(cl.exp_dir) 10*mean(cl.inst_dec)-5 ...
+%         10*mean(cl.lfcons)-15 2*mean(cl.seqnum)];
+    
+%     scatter(1:numel(clfeat), clfeat, 'filled', 'DisplayName', num2str(i), ... 
+%         'MarkerEdgeColor', cmap(i,:),'MarkerFaceColor', cmap(i,:)); hold on;
+
+%     p = plot(1:numel(clfeat), clfeat, 'LineWidth', 0.1, 'color', cmap(i,:), 'DisplayName', '');
+%         p.Color(4) = 0.2;
+    
+    sh = (i-1)*0.1;
+    xax = [1:numF] + sh;
+    
+    scatter(xax, yMean, 'filled', 'DisplayName', num2str(i), ... 
+        'MarkerEdgeColor', cmap(i,:),'MarkerFaceColor', cmap(i,:)); hold on;
+    
+    scatter(xax, yCI95(1,:)+yMean,'^', 'DisplayName', num2str(i), ... 
+        'MarkerEdgeColor', cmap(i,:),'MarkerFaceColor', cmap(i,:));
+    
+    scatter(xax, yCI95(2,:)+yMean,'v', 'DisplayName', num2str(i), ... 
+        'MarkerEdgeColor', cmap(i,:),'MarkerFaceColor', cmap(i,:));
+    
+    
+    p = plot(xax, yCI95(1,:)+yMean, 'LineWidth', 0.1, 'color', cmap(i,:), 'DisplayName', '');
+    p.Color(4) = 0.2;
+    p = plot(xax, yCI95(2,:)+yMean, 'LineWidth', 0.1, 'color', cmap(i,:), 'DisplayName', '');
+    p.Color(4) = 0.2;
+    p = plot(xax, yMean, 'LineWidth', 0.1, 'color', cmap(i,:), 'DisplayName', '');
+    p.Color(4) = 0.2;
+    
+    
+    
+    
+end
+
+% ax = gca;
+% ax.XAxis.XTickLable = varnames;
+% ax.XAxis.TickLabelRotation = 30;
+
+% xticks(1:numF);
+set(gca,'xtick',[1:numF],'xticklabel',varnames,...
+    'TickLabelInterpreter','None', 'XTickLabelRotation',30);
+
+
+hold off; grid on; box on;
+
+% legend(num2str([1:k]'));
+legend;
+
+xlabel('features')
+sgtitle(sprintf('Cluster (k=%d) Visualization with 95%% Conf Interval', k), 'Interpreter','None');
+
+
+%% Plot Fstr-vel Shapes
+
+ks = unique(idx_temp);
+
+for j = 1:numel(ks)
+    
+    sl = primtable(idx_temp==ks(j),:);
+    figure(300+j); 
+    subplot(1,2,1);hold on;
+    for i=1:size(sl,1)
+       prim = sl.prim(i); 
+       t = prim.time_steps - prim.time_steps(1);
+       t = t/t(end);
+       
+       plot(t, prim.fstr(1:numel(t)),'b', 'LineWidth', 0.5);
+       
+    end
+    
+    ylim([-50, 70])
+    hold off; grid on;
+    ylabel('F_{str}')
+    subtitle('F_{str}');
+    
+    
+%     figure(400+j);
+    subplot(1,2,2);
+    hold on;
+    for i=1:size(sl,1)
+       prim = sl.prim(i); 
+       t = prim.time_steps - prim.time_steps(1);
+       t = t/t(end);
+
+       plot(t, (prim.vel(1:numel(t),2)),'b', 'LineWidth', 0.5);
+       
+    end
+    ylim([-0.7, 0.7])
+    ylabel('v_{y}'); grid on;
+    subtitle('v_{y}');
+    sgtitle(sprintf('cluster: %d #points: %d', ks(j), size(sl,1)));
+    hold off;
+%     title(sprintf('cluster: %d', ks(j)));
+%     hold off; grid on;ylabel('v_{y}');
+end
+
 
